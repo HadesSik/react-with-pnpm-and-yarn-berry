429773c5f703ba9a4ba26af826b6880d87b7328a
lisiver <developer.lisiver@gmail.com>
2023-02-15 22:53:57 +0900

    init(emotion): react-router-dom과 @emotion global을 세팅합니다

---
+++ src/modules/styles/reset.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+const resetCss = css`
+  html,
+  body {
+    font-size: 16px;
+    font-family: Pretendard, Noto Sans KR, '돋움', sans-serif;
+  }
+`;
+
+export default resetCss;
---
+++ src/pages/Main.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+const MainPage = () => {
+  return <StyledWrapper>children</StyledWrapper>;
+};
+
+export default MainPage;
+
+const StyledWrapper = styled.div``;
